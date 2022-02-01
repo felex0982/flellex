e349ca675a2b4b8646e463c1c12cc71089886101
Felix Wagner <fw@thepeaklab.com>
2022-02-01 18:01:39 +0100

    init: remix & tailwind

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-xl font-bold">Moin Remix & Tailwind 👋</h1>
+    </div>
+  );
+}
